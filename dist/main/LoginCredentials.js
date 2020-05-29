d63beda87cf3d97cecc98703de5b21824019c381
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-05-29 07:07:07 +0200

    add dist folder

---
+++ dist/main/LoginCredentials.js
@@ -0,0 +1,20 @@
+import * as DockerName from 'docker-names';
+import { BaseUtils } from 'phovea_core';
+export class LoginCredentials {
+    static create(_menu, dialog) {
+        // generate random username
+        const cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)randomCredentials\s*=\s*([^;]*).*$)|^.*$/, '$1');
+        let username = DockerName.getRandomName();
+        let password = BaseUtils.randomId(6);
+        if (cookieValue) {
+            // restore old value
+            [username, password] = cookieValue.split('@');
+        }
+        // store for next time
+        const maxAge = 2 * 7 * 24 * 60 * 60; // 2 weeks in seconds
+        document.cookie = `randomCredentials=${username}@${password};max-age=${maxAge}`;
+        dialog.querySelector('input#login_username').value = username;
+        dialog.querySelector('input#login_password').value = password;
+    }
+}
+//# sourceMappingURL=LoginCredentials.js.map

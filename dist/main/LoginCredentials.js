cf05fa38681b4250fe55e4958c7b491f08d88aec
Klaus Eckelt <Klaus.Eckelt@jku.at>
2020-11-18 19:17:43 +0100

    SameSite attribute for rnd credentials cookies #34

--- dist/main/LoginCredentials.js
+++ dist/main/LoginCredentials.js
@@ -17,7 +17,7 @@ export class LoginCredentials {
         }
         // store for next time
         const maxAge = 2 * 7 * 24 * 60 * 60; // 2 weeks in seconds
-        document.cookie = `randomCredentials=${username}@${password};max-age=${maxAge}`;
+        document.cookie = `randomCredentials=${username}@${password};max-age=${maxAge};SameSite=Strict`;
         dialog.querySelector('input#login_username').value = username;
         dialog.querySelector('input#login_password').value = password;
     }
--- src/main/LoginCredentials.ts
+++ src/main/LoginCredentials.ts
@@ -21,7 +21,7 @@ export class LoginCredentials {
 
     // store for next time
     const maxAge = 2 * 7 * 24 * 60 * 60; // 2 weeks in seconds
-    document.cookie = `randomCredentials=${username}@${password};max-age=${maxAge}`;
+    document.cookie = `randomCredentials=${username}@${password};max-age=${maxAge};SameSite=Strict`;
 
     (<HTMLInputElement>dialog.querySelector('input#login_username')).value = username;
     (<HTMLInputElement>dialog.querySelector('input#login_password')).value = password;

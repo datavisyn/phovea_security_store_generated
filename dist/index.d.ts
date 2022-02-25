1a83d87c2433f1c9b318808a0fc87745b36404e5
dvdanielrehberger <70503076+dvdanielrehberger@users.noreply.github.com>
2022-02-25 13:38:16 +0100

    ESLint (#48)
    * Init
    
    * First linted version
    
    * Renamed tests file

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,2 @@
 export * from './main';
+//# sourceMappingURL=index.d.ts.map
--- dist/main/LoginCredentials.d.ts
+++ dist/main/LoginCredentials.d.ts
@@ -1,3 +1,4 @@
 export declare class LoginCredentials {
     static create(_menu: HTMLElement, dialog: HTMLElement): Promise<void>;
 }
+//# sourceMappingURL=LoginCredentials.d.ts.map
